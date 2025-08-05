0d28351102d141804862087b3aa4bce570daac9c
kirill_blinov <blinovk955@gmail.com>
2025-08-05 13:48:46 +0300

    mvp 5 | add requests to statement service

--- gateway/src/main/java/neo/study/gateway/controller/GatewayController.java
+++ gateway/src/main/java/neo/study/gateway/controller/GatewayController.java
@@ -1,5 +1,54 @@
 package neo.study.gateway.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import neo.study.deal.dto.FinishRegistrationRequestDto;
+import neo.study.deal.dto.LoanOfferDto;
+import neo.study.deal.dto.LoanStatementRequestDto;
+import neo.study.gateway.service.GatewayService;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequiredArgsConstructor
 public class GatewayController {
+    private final GatewayService gatewayService;
+
+    @PostMapping("/statement")
+    public ResponseEntity<List<LoanOfferDto>> processStatement(@RequestBody LoanStatementRequestDto requestDto) {
+        return ResponseEntity.ok(gatewayService.processStatement(requestDto));
+    }
+
+    @PostMapping("/statement/offer")
+    public void selectOffer(@RequestBody LoanOfferDto requestDto) {
+        gatewayService.selectOffer(requestDto);
+    }
+
+    @PostMapping("/deal/calculate/{statementId}")
+    public void completeRegistration(@PathVariable String statementId,
+            @RequestBody FinishRegistrationRequestDto requestRegistration) {
+
+    }
+
+    @PostMapping("/deal/document/{statementId}/send")
+    public void sendDocuments(@PathVariable String statementId) {
+
+    }
+
+    @PostMapping("/deal/document/{statementId}/sign")
+    public void signDocuments(@PathVariable String statementId) {
+
+    }
+
+    @PostMapping("/deal/document/{statementId}/code")
+    public void codeDocuments(@PathVariable String statementId) {
+
+    }
 
 }
--- gateway/src/main/java/neo/study/gateway/service/GatewayService.java
+++ gateway/src/main/java/neo/study/gateway/service/GatewayService.java
@@ -1,5 +1,45 @@
 package neo.study.gateway.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClient;
+
+import lombok.RequiredArgsConstructor;
+import neo.study.deal.dto.LoanOfferDto;
+import neo.study.deal.dto.LoanStatementRequestDto;
+
+@Service
+@RequiredArgsConstructor
 public class GatewayService {
 
+    @Value("${services.statement.process-statement-api}")
+    private String processStatementApi;
+
+    @Value("${services.statement.select-offer-api}")
+    private String selectOfferApi;
+
+    private final RestClient restClientToStatement;
+
+    public List<LoanOfferDto> processStatement(LoanStatementRequestDto requestDto) {
+        var offers = Optional.ofNullable(getLoanOffers(requestDto))
+                .orElseThrow(() -> new HttpClientErrorException(HttpStatus.BAD_REQUEST,
+                        "The service returned an empty list of offers"));
+        return offers;
+    }
+
+    public void selectOffer(LoanOfferDto selectedOffer) {
+        restClientToStatement.post().uri(selectOfferApi).body(selectedOffer).retrieve().toBodilessEntity();
+    }
+
+    private List<LoanOfferDto> getLoanOffers(LoanStatementRequestDto requestDto) {
+        return restClientToStatement.post().uri(processStatementApi).body(requestDto).retrieve()
+                .body(new ParameterizedTypeReference<>() {
+                });
+    }
 }
