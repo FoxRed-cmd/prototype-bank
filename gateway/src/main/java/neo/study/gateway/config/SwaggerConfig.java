bb1e1e8d95802969dd3eb6e1bdb3405cf04b7d78
kirill_blinov <blinovk955@gmail.com>
2025-08-05 12:46:48 +0300

    mvp 5 | add base config; add openapi generator

--- gateway/src/main/java/neo/study/gateway/config/SwaggerConfig.java
+++ gateway/src/main/java/neo/study/gateway/config/SwaggerConfig.java
@@ -1,5 +1,16 @@
 package neo.study.gateway.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
 public class SwaggerConfig {
 
+    @Bean
+    OpenAPI configureOpenAPI() {
+        return new OpenAPI().info(new Info().title("Gateway API").version("1.0.0"));
+    }
 }
